0ec5d0ebb50d0a9365bc426a72e375c3553e89f1
sungchilll <csk2751@gmail.com>
2024-12-27 15:49:39 +0900

    add feat:crud

--- src/main/java/actions/AppWithActionsApplication.java
+++ src/main/java/actions/AppWithActionsApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class AppWithActionsApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(AppWithActionsApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(AppWithActionsApplication.class, args);
+    }
 }
